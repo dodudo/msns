eeccc928547ddfe017ac5035c60037da7c30e95c
“ <dudamaode@gmail.com>
2020-03-08 21:29:50 +0800

    add .gitignore

--- msns-dynamic/msns-dynamic-service/src/main/java/com/dxg/msns/dynamic/mapper/DynamicTypeMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dxg.msns.dynamic.mapper;
-
-import com.dxg.msns.dynamic.pojo.DynamicType;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface DynamicTypeMapper extends Mapper<DynamicType> {
-}
--- msns-music/msns-music-service/src/main/java/com/dxg/msns/music/mapper/MusicTypeMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dxg.msns.music.mapper;
-
-import com.dxg.msns.music.pojo.MusicType;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface MusicTypeMapper extends Mapper<MusicType> {
-}
